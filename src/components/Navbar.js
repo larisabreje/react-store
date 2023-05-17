9a1e466a709b09a689d25cd62c1bbf5a4426acf3
Larisa Breje <larisa@breje.ro>
2023-05-17 22:15:42 +0200

    adding styles to categories and enter functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,35 +2,49 @@ import React from 'react';
 import { useContext } from 'react';
 import styles from './Styles/navbar.module.css';
 import { globalContext } from './Context/Context';
+import { FaUser } from 'react-icons/fa';
+import { FiSearch } from 'react-icons/fi';
 
 const Navbar = () => {
   const { currentData, inputSearch, dataSearch } = useContext(globalContext);
   const [data] = currentData;
   const [search, setSearch] = inputSearch;
   const [, setSendDataSearch] = dataSearch;
+  const handleKeyPress = e => {
+    if (e.key === 'Enter') {
+      setSendDataSearch(`/search?q=${search}`);
+    }
+  };
+  const capitalizeFirstLetter = value => {
+    return value.charAt(0).toUpperCase() + value.slice(1);
+  };
   return (
     <div className={styles.Navbar}>
       {data.map((product, index) => {
         if (index % 5 === 0) {
           return (
             <div className={styles.Category} key={index}>
-              {product.category}
+              {capitalizeFirstLetter(product.category)}
             </div>
           );
         }
       })}
-      <input
-        type="text"
-        className={styles.Search}
-        placeholder="Search"
-        value={search}
-        onChange={e => {
-          setSearch(e.target.value);
-        }}
-      />
-      <button onClick={() => setSendDataSearch(`/search?q=${search}`)}>
-        Search
-      </button>
+      <div className={styles.SearchContainer}>
+        <input
+          type="text"
+          className={styles.Search}
+          placeholder="Search"
+          value={search}
+          onChange={e => {
+            setSearch(e.target.value);
+          }}
+          onKeyDown={handleKeyPress}
+        />
+        <FiSearch
+          className={styles.SearchIcon}
+          onClick={() => setSendDataSearch(`/search?q=${search}`)}
+        />
+      </div>
     </div>
   );
 };
