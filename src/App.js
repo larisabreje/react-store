02f3d060645c7d5cacbceeda4fadd5dd0e4af9f5
Larisa Breje <larisa@breje.ro>
2023-05-17 18:31:57 +0200

    Adding navbar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import logo from "./logo.svg";
-import "./App.css";
-import { Route, Routes } from "react-router-dom";
-import Home from "./components/Home";
-import Product from "./components/Product";
-import Cart from "./components/Cart";
+import logo from './logo.svg';
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import Product from './components/Product';
+import Cart from './components/Cart';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/product/:id" element={<Product />} />
-      <Route path="/cart" element={<Cart />} />
-    </Routes>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/product/:id" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </div>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,55 @@
-import React from "react";
+import React from 'react';
+import { useState, useEffect } from 'react';
+import styles from './Styles/navbar.module.css';
 
 const Navbar = () => {
-  return <div></div>;
+  const [data, setData] = useState([]);
+  const [title, setTitle] = useState('');
+  const getData = async () => {
+    try {
+      const response = await fetch(`https://dummyjson.com/products`);
+      const result = await response.json();
+      setData(result.products);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <div className={styles.Navbar}>
+      {data.map((product, index) => {
+        if (index % 5 === 0) {
+          return (
+            <div className={styles.Category} key={index}>
+              {product.category}
+            </div>
+          );
+        }
+      })}
+      <input
+        type="text"
+        className={styles.Search}
+        placeholder="Search"
+        value={title}
+        onChange={e => {
+          setTitle(e.target.value);
+          setData(
+            data.filter(product => {
+              if (product.title === title) {
+                return true;
+              }
+              return false;
+            })
+          );
+          console.log(data);
+        }}
+      />
+    </div>
+  );
 };
 
 export default Navbar;
